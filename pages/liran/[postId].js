7b1f8becbb4296b08d97384722ec6b449037f102
Liran <iliran11@gmail.com>
2022-04-07 14:32:58 +0300

    translations

--- pages/liran/[postId].js
+++ pages/liran/[postId].js
@@ -1,9 +1,13 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 import React from "react";
 
 function HomePage(props) {
   const [counter, setCounter] = React.useState(1);
+  const { t } = useTranslation();
   return (
     <div>
+      <p>Translation: {t("hello")}</p>
       <p>Counter: {counter}</p>
       <p>Date: {props.text}</p>
       <p>Id: {props.id}</p>
@@ -12,26 +16,31 @@ function HomePage(props) {
   );
 }
 
-export async function getStaticPaths() {
+export async function getStaticPaths(ctx) {
   console.log("get static props");
   return {
     paths: [
-      { params: { postId: "1" } },
-      { params: { postId: "2" } },
-      { params: { postId: "3" } },
-      { params: { postId: "4" } },
+      { params: { postId: "1" }, locale: "en" },
+      { params: { postId: "2" }, locale: "en" },
+      { params: { postId: "3" }, locale: "en" },
+      { params: { postId: "4" }, locale: "en" },
+      { params: { postId: "1" }, locale: "de" },
+      { params: { postId: "2" }, locale: "de" },
+      { params: { postId: "3" }, locale: "de" },
+      { params: { postId: "4" }, locale: "de" },
     ],
     fallback: true, // false or 'blocking'
   };
 }
 
-export async function getStaticProps(context) {
-  console.log("get static paths", context.params.id);
+export async function getStaticProps(ctx) {
+  console.log("get static paths", ctx.params.id);
   return {
     revalidate: 10,
     props: {
       text: new Date().toString(),
-      id: context.params.postId,
+      id: ctx.params.postId,
+      ...(await serverSideTranslations(ctx.locale, ["common"])),
     }, // will be passed to the page component as props
   };
 }
